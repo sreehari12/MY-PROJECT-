6bb4ff5cbbf79f649fda8d4219ee725e0d5a6831
sreehari12 <sreeharisurendran96@gmail.com>
2017-04-05 10:14:37 +0530

    Update prgm.c
    product function added

--- prgm.c
+++ prgm.c
@@ -3,12 +3,14 @@ Void main()
 {
   int b;
   int a;
-  int c;
+  int c,s;
   printf("first program !!!!!!");
   printf("enter the first number");
   scanf("%d",&b);
   printf("enter the second number");
   scanf("%d",&a);
   c=a+b;
+  s=a*b
   printf("sum is ",c);
+  printf("product is ",s)
 }
